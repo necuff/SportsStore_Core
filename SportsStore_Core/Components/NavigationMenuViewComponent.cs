f769e4c2e46a43e87e373e3ab0d769de9065291b
mzaykov <zaykov@inexika.com>
2023-01-11 15:39:25 +0700

    Добавлено меню с категориями

---
+++ SportsStore_Core/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore_Core.Models;
+using System.Linq;
+
+namespace SportsStore_Core.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+
+        public NavigationMenuViewComponent(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
