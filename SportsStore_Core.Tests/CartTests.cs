2adf5a8d854559167b428b079dedb5b77fb2fc87
mzaykov <zaykov@inexika.com>
2023-01-17 15:36:37 +0700

    добавлены тесты корзины на очистку и подсчет стоимости

--- SportsStore_Core.Tests/CartTests.cs
+++ SportsStore_Core.Tests/CartTests.cs
@@ -45,5 +45,58 @@ namespace SportsStore_Core.Tests
             Assert.Equal(11, results[0].Quantity);
             Assert.Equal(1, results[1].Quantity);
         }
+
+        [Fact]
+        public void Can_Remove_Line()
+        {
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Product p3 = new Product { ProductID = 3, Name = "P3" };
+
+            Cart target = new Cart();
+
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 2);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+
+            target.RemoveLine(p2);
+            
+            Assert.Equal(0, target.Lines.Where(c => c.Product == p2).Count());
+            Assert.Equal(2, target.Lines.Count());
+        }
+
+        [Fact]
+        public void Calculate_Cart_Total()
+        {
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            Cart target = new Cart();
+
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 3);
+
+            Decimal result = target.ComputeTotalValue();
+
+            Assert.Equal(450M, result);
+        }
+
+        [Fact]
+        public void Can_ClearContents()
+        {
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            Cart target = new Cart();
+
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+
+            target.Clear();
+
+            Assert.Equal(0, target.Lines.Count());
+        }
     }
 }
