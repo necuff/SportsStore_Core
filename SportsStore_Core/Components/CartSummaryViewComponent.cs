5b886918cdb56b5ce2914e24f95d8264198b786f
mzaykov <zaykov@inexika.com>
2023-01-18 17:32:29 +0700

    доработка корзины

---
+++ SportsStore_Core/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore_Core.Models;
+
+namespace SportsStore_Core.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
